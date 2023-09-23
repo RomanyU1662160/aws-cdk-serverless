642bea1128653203b8548e9fcda57cbdee77d921
RomanySefen-DLG <125323898+RomanySefen-DLG@users.noreply.github.com>
2023-09-23 03:08:39 +0100

    add validation to spaces scheme

--- src/infra/services/shared/DataValidator/spacesSchemaValidator.ts
+++ src/infra/services/shared/DataValidator/spacesSchemaValidator.ts
@@ -1,17 +1,18 @@
-import { Space } from "../../../../types/common";
-import { JsonError, MissingAttributeError } from "../handleErrors";
+import { Space } from '../../../../types/common';
+import { JsonError, MissingAttributeError } from '../handleErrors';
 
+// Add validation for the space schema
 export const validateSpaceSchema = (space: Space) => {
   const { id, name, location } = space;
 
   if (!id) {
-    throw new MissingAttributeError("id");
+    throw new MissingAttributeError('id');
   }
   if (!name) {
-    throw new MissingAttributeError("name");
+    throw new MissingAttributeError('name');
   }
   if (!location) {
-    throw new MissingAttributeError("location");
+    throw new MissingAttributeError('location');
   }
 };
 
