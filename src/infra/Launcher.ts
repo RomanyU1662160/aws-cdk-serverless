4561e5949c879ecab4ac7bc11e0022d3f362c63b
Romany Sefen <romanyfayiez@hotmail.com>
2023-09-23 02:39:53 +0100

    rebuild app

--- src/infra/Launcher.ts
+++ src/infra/Launcher.ts
@@ -1,26 +1,29 @@
-import * as cdk from "aws-cdk-lib";
-import { DataStack } from "./stacks/DataStack";
-import { LambdaStack } from "./stacks/LambdaStack";
-import { APIStack } from "./stacks/APIStack";
-import { AuthStack } from "./stacks/Authstack";
+import * as cdk from 'aws-cdk-lib';
+import { DataStack } from './stacks/DataStack';
+import { LambdaStack } from './stacks/LambdaStack';
+import { APIStack } from './stacks/APIStack';
+import { AuthStack } from './stacks/Authstack';
 
 const app = new cdk.App();
 
-const authStack = new AuthStack(app, "AuthStack", {
+const authStack = new AuthStack(app, 'AuthStack', {
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
+  },
+});
+const addMethod = (num1: number, num2: number) => {
+  return num1 + num2;
+};
+
+const dataStack = new DataStack(app, 'DataStack', {
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
     region: process.env.CDK_DEFAULT_REGION,
   },
 });
 
-const dataStack = new DataStack(app, "DataStack", {
-  env: {
-    account: process.env.CDK_DEFAULT_ACCOUNT,
-    region: process.env.CDK_DEFAULT_REGION,
-  },
-});
-
-const lambdaStack = new LambdaStack(app, "LambdaStack", {
+const lambdaStack = new LambdaStack(app, 'LambdaStack', {
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
     region: process.env.CDK_DEFAULT_REGION,
@@ -28,10 +31,10 @@ const lambdaStack = new LambdaStack(app, "LambdaStack", {
   table: dataStack.table,
 });
 
-const apiGatewayStack = new APIStack(app, "ApiGatewayLambda", {
+const apiGatewayStack = new APIStack(app, 'ApiGatewayLambda', {
   spacesLambdaIntegration: lambdaStack.SpacesLambdaIntegration,
   userCognitoPool: authStack.UserPool,
-  description: "API gateway to call lambda function",
+  description: 'API gateway to call lambda function',
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
     region: process.env.CDK_DEFAULT_REGION,
